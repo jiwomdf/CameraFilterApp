f9401377d3b9b055b1906cc27d2c8166e64a547e
jiwomdf <mdf.jiwo@gmail.com>
2023-09-14 23:18:23 +0700

    now can display camera

--- app/src/main/java/com/katilijiwoadiwiyono/filterrecord/MainActivity.kt
+++ app/src/main/java/com/katilijiwoadiwiyono/filterrecord/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.katilijiwoadiwiyono.filterrecord
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -11,6 +14,8 @@ import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
 import androidx.camera.video.QualitySelector
 import androidx.camera.video.Recorder
 import androidx.camera.video.Recording
@@ -19,8 +24,11 @@ import androidx.camera.video.VideoRecordEvent
 import androidx.concurrent.futures.await
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.util.Consumer
 import androidx.core.view.updateLayoutParams
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenCreated
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.katilijiwoadiwiyono.filterrecord.adapter.GenericListAdapter
 import com.katilijiwoadiwiyono.filterrecord.common.BaseActivity
@@ -33,7 +41,10 @@ import com.katilijiwoadiwiyono.filterrecord.utils.getAspectRatioString
 import com.katilijiwoadiwiyono.filterrecord.utils.getNameString
 import com.katilijiwoadiwiyono.filterrecord.utils.requestPermissionLauncher
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -48,13 +59,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private val cameraPermissionLauncher = requestPermissionLauncher(
         onPermissionGranted = {
-            showToast("camera granted", Toast.LENGTH_SHORT)
+            initCamera()
         },
         onPermissionDenied = {
             showToast("Permission is Required", Toast.LENGTH_SHORT)
         }
     )
 
+    private val captureLiveStatus = MutableLiveData<String>()
     private val cameraCapabilities = mutableListOf<CameraCapability>()
 
     private lateinit var videoCapture: VideoCapture<Recorder>
@@ -75,13 +87,48 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     }
 
+
     override fun getViewBinding(): ActivityMainBinding =
         ActivityMainBinding.inflate(layoutInflater)
+
+    /**
+     * Query and cache this platform's camera capabilities, run only once.
+     */
+    init {
+        enumerationDeferred = lifecycleScope.async {
+            whenCreated {
+                val provider = ProcessCameraProvider.getInstance(this@MainActivity).await()
+
+                provider.unbindAll()
+                for (camSelector in arrayOf(
+                    CameraSelector.DEFAULT_BACK_CAMERA,
+                    CameraSelector.DEFAULT_FRONT_CAMERA
+                )) {
+                    try {
+                        // just get the camera.cameraInfo to query capabilities
+                        // we are not binding anything here.
+                        if (provider.hasCamera(camSelector)) {
+                            val camera = provider.bindToLifecycle(this@MainActivity, camSelector)
+                            QualitySelector
+                                .getSupportedQualities(camera.cameraInfo)
+                                .filter { quality ->
+                                    listOf(Quality.UHD, Quality.FHD, Quality.HD, Quality.SD)
+                                        .contains(quality)
+                                }.also {
+                                    cameraCapabilities.add(CameraCapability(camSelector, it))
+                                }
+                        }
+                    } catch (exc: java.lang.Exception) {
+                        Log.e(TAG, "Camera Face $camSelector is not supported")
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
 
         //checkStoragePermission()
         checkCameraPermission()
@@ -302,8 +349,207 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             permission = Manifest.permission.CAMERA,
             launcher = cameraPermissionLauncher,
             onPermissionGranted = {
-
+                initCamera()
             }
         )
+    }
+
+    /**
+     * One time initialize for CameraFragment (as a part of fragment layout's creation process).
+     * This function performs the following:
+     *   - initialize but disable all UI controls except the Quality selection.
+     *   - set up the Quality selection recycler view.
+     *   - bind use cases to a lifecycle camera, enable UI controls.
+     */
+    private fun initCamera() {
+        initializeUI()
+        lifecycleScope.launch {
+            if (enumerationDeferred != null) {
+                enumerationDeferred!!.await()
+                enumerationDeferred = null
+            }
+            initializeQualitySectionsUI()
+
+            bindCaptureUseCase()
+        }
+    }
+
+    /**
+     * Initialize UI. Preview and Capture actions are configured in this function.
+     * Note that preview and capture are both initialized either by UI or CameraX callbacks
+     * (except the very 1st time upon entering to this fragment in onCreateView()
+     */
+    @SuppressLint("ClickableViewAccessibility", "MissingPermission")
+    private fun initializeUI() {
+        binding.cameraButton.apply {
+            setOnClickListener {
+                cameraIndex = (cameraIndex + 1) % cameraCapabilities.size
+                // camera device change is in effect instantly:
+                //   - reset quality selection
+                //   - restart preview
+                qualityIndex = DEFAULT_QUALITY_IDX
+                initializeQualitySectionsUI()
+                enableUI(false)
+                lifecycleScope.launch {
+                    bindCaptureUseCase()
+                }
+            }
+            isEnabled = false
+        }
+
+        // audioEnabled by default is disabled.
+        binding.audioSelection.isChecked = audioEnabled
+        binding.audioSelection.setOnClickListener {
+            audioEnabled = binding.audioSelection.isChecked
+        }
+
+        // React to user touching the capture button
+        binding.captureButton.apply {
+            setOnClickListener {
+                if (!this@MainActivity::recordingState.isInitialized ||
+                    recordingState is VideoRecordEvent.Finalize)
+                {
+                    enableUI(false)  // Our eventListener will turn on the Recording UI.
+                    startRecording()
+                } else {
+                    when (recordingState) {
+                        is VideoRecordEvent.Start -> {
+                            currentRecording?.pause()
+                            binding.stopButton.visibility = View.VISIBLE
+                        }
+                        is VideoRecordEvent.Pause -> currentRecording?.resume()
+                        is VideoRecordEvent.Resume -> currentRecording?.pause()
+                        else -> throw IllegalStateException("recordingState in unknown state")
+                    }
+                }
+            }
+            isEnabled = false
+        }
+
+        binding.stopButton.apply {
+            setOnClickListener {
+                // stopping: hide it after getting a click before we go to viewing fragment
+                binding.stopButton.visibility = View.INVISIBLE
+                if (currentRecording == null || recordingState is VideoRecordEvent.Finalize) {
+                    return@setOnClickListener
+                }
+
+                val recording = currentRecording
+                if (recording != null) {
+                    recording.stop()
+                    currentRecording = null
+                }
+                binding.captureButton.setImageResource(R.drawable.ic_start)
+            }
+            // ensure the stop button is initialized disabled & invisible
+            visibility = View.INVISIBLE
+            isEnabled = false
+        }
+
+        captureLiveStatus.observe(this) {
+            binding.captureStatus.apply {
+                post { text = it }
+            }
+        }
+        captureLiveStatus.value = getString(R.string.Idle)
+    }
+
+    /**
+     * CaptureEvent listener.
+     */
+    private val captureListener = Consumer<VideoRecordEvent> { event ->
+        // cache the recording state
+        if (event !is VideoRecordEvent.Status)
+            recordingState = event
+
+        updateUI(event)
+
+        if (event is VideoRecordEvent.Finalize) {
+            // display the captured video
+            lifecycleScope.launch {
+                //TODO:JIWO Navigate
+//                navController.navigate(
+//                    CaptureFragmentDirections.actionCaptureToVideoViewer(
+//                        event.outputResults.outputUri
+//                    )
+//                )
+            }
+        }
+    }
+
+    /**
+     * UpdateUI according to CameraX VideoRecordEvent type:
+     *   - user starts capture.
+     *   - this app disables all UI selections.
+     *   - this app enables capture run-time UI (pause/resume/stop).
+     *   - user controls recording with run-time UI, eventually tap "stop" to end.
+     *   - this app informs CameraX recording to stop with recording.stop() (or recording.close()).
+     *   - CameraX notify this app that the recording is indeed stopped, with the Finalize event.
+     *   - this app starts VideoViewer fragment to view the captured result.
+     */
+    private fun updateUI(event: VideoRecordEvent) {
+        val state = if (event is VideoRecordEvent.Status) recordingState.getNameString()
+        else event.getNameString()
+        when (event) {
+            is VideoRecordEvent.Status -> {
+                // placeholder: we update the UI with new status after this when() block,
+                // nothing needs to do here.
+            }
+            is VideoRecordEvent.Start -> {
+                showUI(UiState.RECORDING, event.getNameString())
+            }
+            is VideoRecordEvent.Finalize-> {
+                showUI(UiState.FINALIZED, event.getNameString())
+            }
+            is VideoRecordEvent.Pause -> {
+                binding.captureButton.setImageResource(R.drawable.ic_resume)
+            }
+            is VideoRecordEvent.Resume -> {
+                binding.captureButton.setImageResource(R.drawable.ic_pause)
+            }
+        }
+
+        val stats = event.recordingStats
+        val size = stats.numBytesRecorded / 1000
+        val time = java.util.concurrent.TimeUnit.NANOSECONDS.toSeconds(stats.recordedDurationNanos)
+        var text = "${state}: recorded ${size}KB, in ${time}second"
+        if(event is VideoRecordEvent.Finalize)
+            text = "${text}\nFile saved to: ${event.outputResults.outputUri}"
+
+        captureLiveStatus.value = text
+        Log.i(TAG, "recording event: $text")
+    }
+
+    /**
+     * Kick start the video recording
+     *   - config Recorder to capture to MediaStoreOutput
+     *   - register RecordEvent Listener
+     *   - apply audio request from user
+     *   - start recording!
+     * After this function, user could start/pause/resume/stop recording and application listens
+     * to VideoRecordEvent for the current recording status.
+     */
+    @SuppressLint("MissingPermission")
+    private fun startRecording() {
+        // create MediaStoreOutputOptions for our recorder: resulting our recording!
+        val name = "CameraX-recording-" +
+                SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                    .format(System.currentTimeMillis()) + ".mp4"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Video.Media.DISPLAY_NAME, name)
+        }
+        val mediaStoreOutput = MediaStoreOutputOptions.Builder(
+            this.contentResolver,
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+
+        // configure Recorder and Start recording to the mediaStoreOutput.
+        currentRecording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutput)
+            .apply { if (audioEnabled) withAudioEnabled() }
+            .start(mainThreadExecutor, captureListener)
+
+        Log.i(TAG, "Recording started")
     }
 }
